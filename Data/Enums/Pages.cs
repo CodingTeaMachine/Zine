b7c740370cefadea8112afc142ef3f945ae819b4
Dékány Márk <codingteamachine@gmail.com>
2023-08-26 12:15:16 +0200

    Created basic pages, and fixed github link

---
+++ Data/Enums/Pages.cs
@@ -0,0 +1,11 @@
+namespace Zine.Data.Enums;
+
+public class Pages
+{
+	public static readonly string Home = "/";
+	public static readonly string Library = "/library";
+	public static readonly string CurrentlyReading = "/currently-reading";
+	public static readonly string Import = "/import";
+	public static readonly string Explore = "/explore";
+	public static readonly string Settings = "/settings";
+}
